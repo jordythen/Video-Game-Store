a6430843d9c5f09be29ea162591363b1fdf8b9bb
Jordy Then <42790442+jordythen@users.noreply.github.com>
2020-07-21 12:24:42 -0400

    added game details service

---
+++ VideoGameShop/src/main/java/com/revature/app/services/GameDetailsServiceImpl.java
@@ -0,0 +1,49 @@
+package com.revature.app.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.revature.app.beans.GameDetails;
+import com.revature.app.data.GameDetailsOracle;
+
+public class GameDetailsServiceImpl implements GameDetailsService {
+
+	private static GameDetailsOracle gdDao;
+
+	@Autowired
+	public GameDetailsServiceImpl() {
+		gdDao = new GameDetailsOracle();
+	}
+	
+	@Override
+	public Integer add(GameDetails t) {
+		// TODO Auto-generated method stub
+		return gdDao.add(t);
+	}
+
+	@Override
+	public GameDetails findById(Integer id) {
+		// TODO Auto-generated method stub
+		return gdDao.getById(id);
+	}
+
+	@Override
+	public List<GameDetails> findAll() {
+		// TODO Auto-generated method stub
+		return gdDao.getAll();
+	}
+
+	@Override
+	public Boolean update(GameDetails t) {
+		// TODO Auto-generated method stub
+		return gdDao.update(t);
+	}
+
+	@Override
+	public Boolean delete(GameDetails t) {
+		// TODO Auto-generated method stub
+		return gdDao.delete(t);
+	}
+
+}
